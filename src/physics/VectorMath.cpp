3a8b9a9db83e9e47ae78e8056a17bed193a421a0
Spider <doctoraaronli@gmail.com>
2026-06-27 18:58:11 +1200

    Update VectorMath.cpp

--- src/physics/VectorMath.cpp
+++ src/physics/VectorMath.cpp
@@ -1,1 +1,8 @@
+#include "VectorMath.hpp"
 
+namespace phageforge::physics {
+
+// Header-only implementation - all functions are inline in the header
+// This .cpp file exists to provide a translation unit for the library
+
+} // namespace phageforge::physics
