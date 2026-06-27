829a3b435ceee14bf29de46ca6ab9208116a0174
Spider <doctoraaronli@gmail.com>
2026-06-27 21:27:06 +1200

    Update Electrostatics.cpp

--- src/physics/Electrostatics.cpp
+++ src/physics/Electrostatics.cpp
@@ -2,11 +2,8 @@
 
 namespace phageforge::physics {
 
-// Static member initialization
-float ElectrostaticsSolver::m_ionic_strength = core::constants::IONIC_STRENGTH;
-float ElectrostaticsSolver::m_temperature = core::constants::TEMPERATURE;
-
 // All functions are implemented inline in the header
 // This .cpp file exists to provide a translation unit for the library
+// No static members to define - they were removed from the header
 
 } // namespace phageforge::physics
